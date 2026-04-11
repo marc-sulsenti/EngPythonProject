8c08f682a5ac985f90703fd1c0ba99f1e9bb84e2
marc-sulsenti <marcdantesulsenti@gmail.com>
2026-04-11 16:48:13 -0400

    Normalizing Values Method

--- functions.py
+++ functions.py
@@ -1,1 +1,15 @@
-#  Helper functions here for things like value normalization, math, risk assessment, etc.
+def normalize_value (value, min_value, max_value):
+    '''
+    This function takes a value and normalizes it to a range of 0 to 1 based on the provided minimum and maximum values.
+
+    Parameters:
+    value (float): The value to be normalized.
+    min_value (float): The minimum value in the range.
+    max_value (float): The maximum value in the range.
+
+    Returns:
+    float: The normalized value between 0 and 1.
+    '''
+    if max_value - min_value == 0:
+        return 0.0  # We want to avoid any division by zero error
+    return (value - min_value) / (max_value - min_value) # Return the normalized value.
